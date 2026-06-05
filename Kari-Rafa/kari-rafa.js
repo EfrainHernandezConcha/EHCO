ccbc7f9429eb5859cfc58f96010cb5370c5c8bff
EfrainHernandezConcha <efrain_concha2@hotmail.com>
2026-06-05 14:33:59 -0600

    png change and scale in js

--- Kari-Rafa/kari-rafa.js
+++ Kari-Rafa/kari-rafa.js
@@ -197,7 +197,7 @@ const fechaObjetivo = new Date("Oct 31, 2026 16:00:00").getTime();
         
         // zoom-in
         gsap.to("#zoom-in-1", {
-            scale: .38, stagger: 0.25, duration: 3,
+            scale: .5, stagger: 0.25, duration: 3,
             scrollTrigger: {
                 trigger: "#zoom-in-1",
                 pin: false,
