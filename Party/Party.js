151a8ef55d49aeaf5a962ebb27d8b8943ff6b9f1
EfrainHernandezConcha <efrain_concha2@hotmail.com>
2026-06-08 23:17:53 -0600

    Party

---
+++ Party/Party.js
@@ -0,0 +1,240 @@
+
+//FUNCION PARA DESAPARECER CUBIERTA
+const Vanish = document.querySelectorAll(".vanish-off");/***deberia ser ID****/
+const buttonVanish = document.getElementById("botonPlay");
+
+buttonVanish.addEventListener("click", () => { /***********/
+
+  Vanish.forEach(Vanish => {
+
+    if(Vanish.classList.contains("vanish-off")){
+
+      Vanish.classList.remove("vanish-off");
+      Vanish.classList.add("vanish-on");
+
+    }
+    /*else{
+
+      Vanish.classList.remove("vanish-on");
+      Vanish.classList.add("vanish-off");
+
+    }*/
+  });
+
+});
+
+//FUNCION INICIAL PARA AJUSTAR CONTENEDOR
+        const container = document.querySelector('.container-img');
+        
+        function logContainerDimensions() {
+            console.log('Container Width:', container.offsetWidth + 'px');
+            console.log('Container Height:', container.offsetHeight + 'px');
+        }
+
+        // Log on load
+        window.addEventListener('load', logContainerDimensions);
+
+        // Log on resize
+        window.addEventListener('resize', logContainerDimensions);
+
+//FUNCION ZOOM-ON-SCROLL
+const zoom = document.querySelectorAll('.zoom');
+function zoomScroll() {
+  const alturaPantalla = window.innerHeight;
+
+  zoom.forEach(el => {
+    const distancia = el.getBoundingClientRect().top;
+
+    if (distancia < alturaPantalla - 80) {
+      el.classList.add('zoom-on');
+    }
+    else {
+        el.classList.remove('zoom-on');
+        el.classList.add('zoom');
+      }
+  });
+}
+window.addEventListener('scroll', zoomScroll);
+
+//FUNCION SLIDE-ON-SCROLL
+const slide = document.querySelectorAll('.left-slide');
+function slideScroll() {
+  const alturaPantalla = window.innerHeight;
+
+  slide.forEach(el => {
+    const distancia = el.getBoundingClientRect().top;
+
+    if (distancia < alturaPantalla - 50) {
+      el.classList.add('left-slide-on');
+    }
+    else {
+        el.classList.remove('left-slide-on')
+      }
+  });
+}
+window.addEventListener('scroll', slideScroll);
+
+
+//FUNCION SLIDE-ON-SCROLL
+const slideRight = document.querySelectorAll('.right-slide');
+function slideRightScroll() {
+  const alturaPantalla = window.innerHeight;
+
+  slideRight.forEach(el => {
+    const distancia = el.getBoundingClientRect().top;
+
+    if (distancia < alturaPantalla - 50) {
+      el.classList.add('right-slide-on');
+    }
+    else {
+        el.classList.remove('right-slide-on')
+      }
+  });
+}
+window.addEventListener('scroll', slideRightScroll);
+
+
+
+
+//REPRODUCTOR DE MUSICA
+const audio = document.getElementById('miAudio');
+const button = document.getElementById('botonPlay');
+
+button.addEventListener('click', function() {
+    if (audio.paused) {
+        audio.play();
+        button.innerHTML = ''; // Cambia a icono de pausa
+    } else {
+        audio.pause();
+        button.innerHTML = ''; // Cambia a icono de play
+    }
+});
+
+
+//CUENTA REGRESIVA 
+
+const fechaObjetivo = new Date("Dec 05, 2026 16:00:00").getTime();
+
+        const actualizarContador = () => {
+
+            const ahora = new Date().getTime();
+            const diferencia = fechaObjetivo - ahora;
+
+            const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+
+            const horas = Math.floor(
+                (diferencia % (1000 * 60 * 60 * 24))
+                / (1000 * 60 * 60)
+            );
+
+            const minutos = Math.floor(
+                (diferencia % (1000 * 60 * 60))
+                / (1000 * 60)
+            );
+
+            const segundos = Math.floor(
+                (diferencia % (1000 * 60))
+                / 1000
+            );
+
+            document.getElementById("dias").innerHTML = dias;
+            document.getElementById("horas").innerHTML = horas;
+            document.getElementById("minutos").innerHTML = minutos;
+            document.getElementById("segundos").innerHTML = segundos;
+
+            // Cuando termina
+            if(diferencia < 0){
+                clearInterval(intervalo);
+
+                document.querySelector(".contador").innerHTML = `
+                    <h1>¡La cuenta terminó!</h1>
+                `;
+            }
+        };
+
+        actualizarContador();
+
+        const intervalo = setInterval(actualizarContador, 1000);
+
+
+
+        const { innerHeight } = window;
+
+        // zoom-out
+        gsap.from("#zoom-out-1", {
+            scale: .5, stagger: 0.25, duration: 1,
+            scrollTrigger: {
+                trigger: "#zoom-out-1",
+                pin: false,
+                end: `+=${innerHeight * 8}`,
+                scrub: 3
+            }
+        });
+
+        // zoom-out
+        gsap.from("#zoom-out-2", {
+          scale: .4, stagger: 0.25, duration: 1,
+          scrollTrigger: {
+              trigger: "#zoom-out-2",
+              pin: false,
+              end: `+=${innerHeight * 12}`,
+              scrub: 3
+          }
+      });
+
+        // zoom-out
+        gsap.from("#zoom-out-3", {
+          scale: .5, stagger: 0.25, duration: 1,
+          scrollTrigger: {
+              trigger: "#zoom-out-3",
+              pin: false,
+              end: `+=${innerHeight * 14}`,
+              scrub: 3
+          }
+      });
+
+
+        
+        // zoom-in
+        gsap.to("#zoom-in-1", {
+            scale: .3, stagger: 0.25, duration: 3,
+            scrollTrigger: {
+                trigger: "#zoom-in-1",
+                pin: false,
+                end: `+=${innerHeight * 5}`,
+                scrub: 3
+            }
+        });
+
+
+        // zoom-in
+        gsap.from("#zoom-in-2", {
+          scale: .6, stagger: 0.25, duration: 3,
+          scrollTrigger: {
+              trigger: "#zoom-in-2",
+              pin: false,
+              end: `+=${innerHeight * 15}`,
+              scrub: 3
+          }
+      });
+
+
+
+//FUNCION APPEAR-ON-SCROLL
+const appear = document.querySelectorAll('.appear');
+function appearScroll() {
+  const alturaPantalla = window.innerHeight;
+
+  appear.forEach(el => {
+    const distancia = el.getBoundingClientRect().top;
+
+    if (distancia < alturaPantalla - 10) {
+      el.classList.add('appear-on');
+    }
+    else {
+        el.classList.remove('appear-on');
+        el.classList.add('appear');
+      }
+  });
+}
+window.addEventListener('scroll', appearScroll);
